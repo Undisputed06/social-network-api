a08efbf3c2d5ebf40cbc041351fd1a986994de62
Gary <52983305+Undisputed06@users.noreply.github.com>
2022-07-21 13:47:23 -0500

    updated User controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,16 +7,6 @@ const UserController = {
     // get all users
     getAllUsers(req, res) {
       User.find({})
-        .populate({
-          path: 'thoughts',
-          select: '-__v'
-        })
-        .populate({
-            path: 'friends',
-            select: '-__v'
-        })
-        .select('-__v')
-        .sort({ _id: -1 })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
           console.log(err);
@@ -27,11 +17,6 @@ const UserController = {
 // get one User by id
     getUserById({ params }, res) {
       User.findOne({ _id: params.id })
-        .populate({
-          path: 'thought',
-          select: '-__v'
-        })
-        .select('-__v')
         .then(dbUserData => {
           if (!dbUserData) {
             res.status(404).json({ message: 'No User found with this id!' });
